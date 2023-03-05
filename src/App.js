e83420e746f59e5517e6a277a5cd039d0d8212f7
speaktosameer <speaktosameernp@gmail.com>
2023-03-05 13:15:08 +0545

    Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import Home from "./pages/Home/Home";
-import {BrowserRouter, Routes, Route } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
 import WhyUs from "./pages/WhyUs/WhyUs";
 import Faq from "./pages/FAQ/Faq";
 import Portfolio from "./pages/Portfolio/Portfolio";
@@ -11,6 +11,7 @@ import OnlineEvent from "./pages/Services/OnlineEvent";
 import SocialMedia from "./pages/Services/SocialMedia";
 import WebsiteDesign from "./pages/Services/WebsiteDesign";
 import WhatsAppAdver from "./pages/Services/WhatsAppAdver";
+import { BrowserRouter } from "react-router-dom";
 
 
 function App() {
@@ -19,18 +20,17 @@ function App() {
       <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home/>}/>
-            <Route path="contact" element={<Contact/>}/>
-            <Route path="about" element={<About/>}/>
-            <Route path="socialrajneeti" element={<WhyUs/>}/>
-            <Route path="faq" element={<Faq/>}/>
-            <Route path="portfolio" element={<Portfolio/>}/>
-            <Route path="digital-marketing" element={<DigitalMarket/>}/>
-            <Route path="online-advertising" element={<OnlineAdvertise/>}/>
-            <Route path="online-event" element={<OnlineEvent/>}/>
-            <Route path="social-media" element={<SocialMedia/>}/>
-            <Route path="website-design"element={<WebsiteDesign/>}/>
-            <Route path="whatsapp-advertise" element={<WhatsAppAdver/>}/>
-
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/socialrajneeti" element={<WhyUs/>}/>
+            <Route path="/faq" element={<Faq/>}/>
+            <Route path="/portfolio" element={<Portfolio/>}/>
+            <Route path="/digital-marketing" element={<DigitalMarket/>}/>
+            <Route path="/online-advertising" element={<OnlineAdvertise/>}/>
+            <Route path="/online-event" element={<OnlineEvent/>}/>
+            <Route path="/social-media" element={<SocialMedia/>}/>
+            <Route path="/website-design"element={<WebsiteDesign/>}/>
+            <Route path="/whatsapp-advertise" element={<WhatsAppAdver/>}/>
           </Routes>
       </BrowserRouter>
     </>
